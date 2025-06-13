a3aab2ca5de73202ee1ca5491fe422e78a362aba
Dev Mohammed Ahmed <137880702+amiin32@users.noreply.github.com>
2025-06-14 00:40:43 +0300

    Neccersarry files

---
+++ main.py
@@ -0,0 +1,30 @@
+import pvp
+import mid_ai
+import smart_ai
+def main():
+    while True:
+        print("==== TIC TAC TOE GAME ====")
+        print("1. Player Vs Player (PVP)")
+        print("2. Player Vs AI (PVAi)")
+        print("3. Quit")
+
+        userChoice = input("Enter your choice: ")
+
+        if userChoice == "1":
+            pvp.start_game()
+        elif userChoice == "2":
+            print("1. Medium")
+            print("2. Smart Ai")
+            aichoice = int(input("Enter your choice: "))
+            if aichoice == 1:
+                mid_ai.start_game()
+            elif aichoice == 2:
+                smart_ai.start_game()
+        elif userChoice == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Try again.")
+            exit()
+
+main()
